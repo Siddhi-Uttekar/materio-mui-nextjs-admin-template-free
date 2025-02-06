c0b604ce3f9806769e26b5e2870ae980aaf356ed
Siddhi-Uttekar <uttekarsiddhi145@gmail.com>
2025-02-06 23:21:44 +0530

    f

--- javascript-version/.eslintrc.js
+++ javascript-version/.eslintrc.js
@@ -5,7 +5,43 @@ module.exports = {
     "prettier"
   ],
   "rules": {
-   
+   '@next/next/google-font-display': 'off',
+
+    // Disable the rule for using preconnect with Google Fonts
+    '@next/next/google-font-preconnect': 'off',
+
+    // Disable rule to enforce inline script ids
+    '@next/next/inline-script-id': 'off',
+
+    // Disable rule for preventing assignment to module variable
+    '@next/next/no-assign-module-variable': 'off',
+
+    // Disable rule for preventing async client components
+    '@next/next/no-async-client-component': 'off',
+
+    // Disable rule for preventing script usage outside of _document.js
+    '@next/next/no-before-interactive-script-outside-document': 'off',
+
+    // Disable rule for preventing usage of <img> elements
+    '@next/next/no-img-element': 'off',
+
+    // Disable rule for preventing custom fonts in pages
+    '@next/next/no-page-custom-font': 'off',
+
+    // Disable rule for preventing usage of <script> in next/head
+    '@next/next/no-script-component-in-head': 'off',
+
+    // Disable rule for preventing duplicate <Head> usage
+    '@next/next/no-duplicate-head': 'off',
+
+    // Disable rule for preventing <title> in document head
+    '@next/next/no-title-in-document-head': 'off',
+
+    // Disable rule for common typos in Next.js's data fetching functions
+    '@next/next/no-typos': 'off',
+
+    // Disable rule for preventing unwanted polyfills
+    '@next/next/no-unwanted-polyfillio': 'off',
       "import/order": "off", // Disables the import order rule
       "padding-line-between-statements": "off", // Disables blank line requirements
 
--- javascript-version/next.config.mjs
+++ javascript-version/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: process.env.BASEPATH
-  ignoreDuringBuilds: true,
+  
 
 }
 
